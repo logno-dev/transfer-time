bbdab6c77d11496574e6f655110bfb6bbdfe2f53
Logan Bunch <logno@Air.local>
2024-06-27 00:45:04 -0700

    added history

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 const form = document.getElementById('form')
 const estimate = document.getElementById('estimate')
 const time = document.getElementById('time')
+const hist = document.getElementById("hist")
+
+const history = []
+if (!!localStorage.length) {
+  history.push(...JSON.parse(localStorage.getItem("history")))
+  updateHistory()
+}
+
+
+function updateHistory() {
+  hist.innerHTML = ""
+  for (let i = history.length - 1; i >= 0; i--) {
+    hist.innerHTML += `<tr><td>${history[i].pounds}</td><td>${history[i].speed}</td><td>${history[i].duration}</td><td>${history[i].time}</td></tr>`
+  }
+
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -8,7 +24,20 @@ form.addEventListener('submit', (e) => {
   const pounds = input.get('pounds')
   const speed = input.get('speed')
   const calculation = pounds / 8.33 / speed / 60
-  console.log(calculation)
-  estimate.textContent = `${Math.floor(calculation)} hours ${Math.round((calculation - Math.floor(calculation)) * 60)} minutes`
-  time.textContent = new Date(new Date().getTime() + calculation * 60 * 60 * 1000).toLocaleTimeString()
+  const estimateValue = `${Math.floor(calculation)} hours ${Math.round((calculation - Math.floor(calculation)) * 60)} minutes`
+  const timeValue = new Date(new Date().getTime() + calculation * 60 * 60 * 1000).toLocaleTimeString()
+
+
+  estimate.textContent = estimateValue
+  time.textContent = timeValue
+
+  history.push({"pounds": pounds, "speed": speed, "duration": estimateValue, "time": timeValue})
+
+  if (history.length === 50) {
+    history.shift()
+  }
+  
+  localStorage.setItem("history", JSON.stringify(history))
+
+  updateHistory()
 })
